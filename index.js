e40153e0b5feca1e505b56d9664dde0da8648743
Arpendu Kumar Garai <Arpendu.KumarGarai@microfocus.com>
2020-06-15 20:26:34 +0530

    moving index.js to root

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../../assets/styles/sass/styles.scss';
-import App from './App';
+import './assets/styles/sass/styles.scss';
+import App from './src/js/App';
 
 ReactDOM.render(
   <App />, document.getElementById('app')
